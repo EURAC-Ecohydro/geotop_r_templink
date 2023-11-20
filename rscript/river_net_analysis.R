e4c702ea1d5e3a2718c686f1f36aeb0db4c3a2bd
Emanuele Cordano <emanuele.cordano@gmail.com>
2023-11-20 10:21:22 +0100

    channel network investigation

---
+++ rscript/river_net_analysis.R
@@ -0,0 +1,25 @@
+
+## Network channal processing from a GEotop (www.geotop.org) simulation
+
+# Author: Emanuele Cordano
+# Date: 2023-11-18
+# License: GPL-3.0
+
+# Load necessary libraries
+library(geotopbricks)
+library(terra) ## using terra package version >=  1.7-59 (fork  devoleped by Emanuele Cordano)
+geotop_tests <- "/home/ecor/local/sw/rendena100/geotop/tests/"
+wpath <- "/home/ecor/local/repos/geotop_r_templink/geotop_simulations/templates/TEST_Venosta_3D_034_template/"
+
+net <- get.geotop.inpts.keyword.value("RiverNetwork",raster=TRUE,wpath=wpath) 
+
+### GEOTOP Examples
+wpath_borden <-paste0(geotop_tests,"3D/Borden05m/")
+net_borden <- get.geotop.inpts.keyword.value("RiverNetwork",raster=TRUE,wpath=wpath_borden) 
+#
+wpath_rendena <-paste0(geotop_tests,"3D/rendena/")
+net_rendena <- get.geotop.inpts.keyword.value("RiverNetwork",raster=TRUE,wpath=wpath_rendena) 
+##
+wpath_uac <-paste0(geotop_tests,"3D/UpperAmmerCatchment/")
+net_uac <- get.geotop.inpts.keyword.value("RiverNetwork",raster=TRUE,wpath=wpath_uac) 
+
